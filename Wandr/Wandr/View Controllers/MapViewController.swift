2d67f6cbc70519d816015257472f1b92be3713c6
Ana <anama118118@gmail.com>
2017-02-27 16:55:15 -0500

    Added actions to addButton and swipGesture to drapUpContainerView

--- Wandr/Wandr/View Controllers/MapViewController.swift
+++ Wandr/Wandr/View Controllers/MapViewController.swift
@@ -22,15 +22,118 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         return locMan
     }()
     
+    var addPostViewShown = false
+    
     let segmentTitles = ["Internal", "Private", "Public"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.red
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.yellow
+
         setupViewHierarchy()
         configureConstraints()
-
+        setupGestures()
+    }
+    
+    // MARK: - Actions
+    func togglePostView(_ sender: UISwipeGestureRecognizer) {
+        switch sender.direction {
+        case UISwipeGestureRecognizerDirection.up where self.addPostViewShown == false:
+            animateSettingsMenu(showPost: self.addPostViewShown, duration: 1.0, dampening: 0.5, springVelocity: 5)
+             self.addPostViewShown = !addPostViewShown
+        case UISwipeGestureRecognizerDirection.down where self.addPostViewShown == true:
+            animateSettingsMenu(showPost: self.addPostViewShown, duration: 1.0, dampening: 0.5, springVelocity: 5)
+             self.addPostViewShown = !addPostViewShown
+        default:
+            print("Not a recognized gesture")
+        }
+    }
+    
+    func animatePostView() {
+        animateSettingsMenu(showPost: self.addPostViewShown, duration: 1.0, dampening: 0.5, springVelocity: 5)
+        self.addPostViewShown = !addPostViewShown
+    }
+    
+    private func animateSettingsMenu(showPost: Bool, duration: TimeInterval, dampening: CGFloat = 0.005, springVelocity: CGFloat = 0.005) {
+        switch self.addPostViewShown {
+        case true:
+            UIView.animate(withDuration: duration, delay: 0.0, usingSpringWithDamping: dampening, initialSpringVelocity: springVelocity, options: .curveEaseOut, animations: {
+                
+                self.dragUpContainerView.snp.remakeConstraints { (view) in
+                    view.leading.equalToSuperview()
+                    view.trailing.equalToSuperview()
+                    view.height.equalToSuperview().multipliedBy(0.5)
+                    view.width.equalToSuperview()
+                }
+                
+                self.cheveronButton.snp.remakeConstraints { (button) in
+                    button.top.equalToSuperview()
+                    button.trailing.equalToSuperview().inset(16)
+                }
+                
+                self.segmentedControlContainerView.snp.remakeConstraints { (view) in
+                    view.top.equalTo(self.cheveronButton.snp.centerY)
+                    view.leading.trailing.equalToSuperview()
+                    view.height.equalToSuperview().multipliedBy(0.175)
+                    view.bottom.equalTo(self.bottomLayoutGuide.snp.top)
+                }
+                
+                self.segmentedControl.snp.remakeConstraints { (control) in
+                    control.top.leading.bottom.trailing.equalToSuperview()
+                }
+                
+                self.postContainerView.snp.remakeConstraints { (view) in
+                    view.top.equalTo(self.segmentedControlContainerView.snp.bottom)
+                    view.leading.trailing.bottom.equalToSuperview()
+                }
+                
+            }, completion: nil)
+            
+        case false:
+           UIView.animate(withDuration: duration, delay: 0.0, usingSpringWithDamping: dampening, initialSpringVelocity: springVelocity, options: .curveEaseOut, animations: {
+            self.dragUpContainerView.snp.remakeConstraints({ (view) in
+                view.leading.trailing.equalToSuperview()
+                view.bottom.equalTo(self.bottomLayoutGuide.snp.top)
+                view.height.equalToSuperview().multipliedBy(0.5)
+                view.width.equalToSuperview()
+            })
+            
+            self.cheveronButton.snp.remakeConstraints { (button) in
+                button.top.equalToSuperview()
+                button.trailing.equalToSuperview().inset(16)
+            }
+            
+            self.segmentedControlContainerView.snp.remakeConstraints({ (view) in
+                view.top.equalTo(self.cheveronButton.snp.centerY)
+                view.leading.trailing.equalToSuperview()
+                view.height.equalToSuperview().multipliedBy(0.175)
+            })
+            
+            self.segmentedControl.snp.remakeConstraints { (control) in
+                control.top.leading.bottom.trailing.equalToSuperview()
+            }
+            
+            self.postContainerView.snp.remakeConstraints { (view) in
+                view.top.equalTo(self.segmentedControlContainerView.snp.bottom)
+                view.leading.trailing.bottom.equalToSuperview()
+            }
+            
+           }, completion: nil)
+            
+        default:
+            print("I don't know what to do")
+        }
+    }
+    
+    // Mark: - Add Gestures
+    private func setupGestures() {
+        let swipeUpGesture = UISwipeGestureRecognizer(target: self, action: #selector(togglePostView))
+        swipeUpGesture.direction = .up
+        self.dragUpContainerView.addGestureRecognizer(swipeUpGesture)
+        
+        let swipeDownGesture = UISwipeGestureRecognizer(target: self, action: #selector(togglePostView))
+        swipeDownGesture.direction = .down
+         self.dragUpContainerView.addGestureRecognizer(swipeDownGesture)
     }
     
     // MARK: - Hierarchy
@@ -62,12 +165,11 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
             view.top.leading.trailing.bottom.equalToSuperview()
         }
         
-        
         //Drag Up Container View
         dragUpContainerView.snp.makeConstraints { (view) in
             view.leading.equalToSuperview()
             view.trailing.equalToSuperview()
-            view.height.equalToSuperview()
+            view.height.equalToSuperview().multipliedBy(0.5)
             view.width.equalToSuperview()
         }
         
@@ -79,7 +181,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         segmentedControlContainerView.snp.makeConstraints { (view) in
             view.top.equalTo(self.cheveronButton.snp.centerY)
             view.leading.trailing.equalToSuperview()
-            view.height.equalToSuperview().multipliedBy(0.1)
+            view.height.equalToSuperview().multipliedBy(0.175)
             view.bottom.equalTo(self.bottomLayoutGuide.snp.top)
         }
         
@@ -145,6 +247,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         button.setTitle("Up", for: .normal)
         button.tintColor = UIColor.yellow
         button.backgroundColor = UIColor.orange
+        button.addTarget(self, action: #selector(animatePostView), for: .touchDragInside)
         return button
     }()
     
